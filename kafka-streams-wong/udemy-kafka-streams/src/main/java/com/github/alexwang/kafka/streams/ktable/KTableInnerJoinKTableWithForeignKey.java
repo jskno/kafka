dc35ebb5642a2cf8f944cb5985b5dbffc893db2a
jskno <lodmnos@gmail.com>
2024-10-13 17:41:20 +0200

    Wong

---
+++ kafka-streams-wong/udemy-kafka-streams/src/main/java/com/github/alexwang/kafka/streams/ktable/KTableInnerJoinKTableWithForeignKey.java
@@ -0,0 +1,9 @@
+package com.github.alexwang.kafka.streams.ktable;
+
+import com.github.alexwang.kafka.streams.windowing.PatientHeartBeatMonitorApp;
+
+/**
+ * @see PatientHeartBeatMonitorApp
+ */
+public class KTableInnerJoinKTableWithForeignKey {
+}
---
+++ kafka-streams-wong/udemy-kafka-streams/src/main/java/com/github/alexwang/kafka/streams/serdes/JsonDeserialization.java
@@ -0,0 +1,24 @@
+package com.github.alexwang.kafka.streams.serdes;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+
+class JsonDeserialization<T> implements Deserializer<T> {
+
+    private Class<T> deserializeClass;
+
+    public JsonDeserialization(Class<T> deserializeClass) {
+        this.deserializeClass = deserializeClass;
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        try {
+            return JsonSerialization.MAPPER.readValue(data, deserializeClass);
+        } catch (Exception e) {
+            //ingore
+        }
+        return null;
+    }
+}
