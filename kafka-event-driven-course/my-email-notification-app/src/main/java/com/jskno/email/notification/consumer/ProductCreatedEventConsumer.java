22617714994ae2a48c5b259f6403677d8056f16d
josecano <jose.cano@europcar.com>
2024-11-25 11:08:43 +0100

    Library

--- kafka-event-driven-course/my-email-notification-app/src/main/java/com/jskno/email/notification/consumer/ProductCreatedEventConsumer.java
+++ kafka-event-driven-course/my-email-notification-app/src/main/java/com/jskno/email/notification/consumer/ProductCreatedEventConsumer.java
@@ -1,5 +1,6 @@
 package com.jskno.email.notification.consumer;
 
+import com.jskno.kafka.event.driven.ProductCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaHandler;
