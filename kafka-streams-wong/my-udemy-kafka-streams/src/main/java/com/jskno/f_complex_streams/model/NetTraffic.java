cd2ea1d4e40967a8e79626e934385d387e0a9503
jskno <lodmnos@gmail.com>
2025-01-28 20:24:24 +0100

    Tumbling Windows

---
+++ kafka-streams-wong/my-udemy-kafka-streams/src/main/java/com/jskno/f_complex_streams/model/NetTraffic.java
@@ -0,0 +1,16 @@
+package com.jskno.f_complex_streams.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NetTraffic {
+
+    private String page;
+    private String remoteAddress;
+}
