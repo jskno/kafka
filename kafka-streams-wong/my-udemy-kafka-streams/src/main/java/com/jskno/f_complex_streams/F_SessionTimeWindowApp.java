70a891d4fb69c6f7d0f9d55dd744c5f9cc987f8c
jskno <lodmnos@gmail.com>
2025-01-29 18:41:46 +0100

    SessionWindows

---
+++ kafka-streams-wong/my-udemy-kafka-streams/src/main/java/com/jskno/f_complex_streams/F_SessionTimeWindowApp.java
@@ -0,0 +1,77 @@
+package com.jskno.f_complex_streams;
+
+import com.jskno.f_complex_streams.model.NetTraffic;
+import com.jskno.serdes.JsonSerdes;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+
+// sudo ./bin/kafka-server-start.sh config/kraft/server.properties
+// ./bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic word-processor-input
+// ./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic word-processor-output --from-beginning
+public class F_SessionTimeWindowApp {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(F_SessionTimeWindowApp.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = buildStreamsProperties();
+        Topology topology = buildTopology();
+
+        KafkaStreams streams = new KafkaStreams(topology, props);
+
+        CountDownLatch latch = new CountDownLatch(1);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            streams.close();
+            latch.countDown();
+            LOGGER.info("The WordProcessorApp is gracefully shutting down");
+        }));
+
+        streams.start();
+        LOGGER.info("WordProcessorApp is started");
+
+        latch.await();
+    }
+
+    private static Properties buildStreamsProperties() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "session-network");
+        props.put(StreamsConfig.STATE_DIR_CONFIG, "/home/jskno/kafka-logs/statestore");
+        props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 3);
+        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
+        props.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 0);
+        return props;
+    }
+
+    private static Topology buildTopology() {
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, NetTraffic> sourceStream = builder.stream(
+                "net-traffic-logs",
+                Consumed.with(Serdes.String(), JsonSerdes.of(NetTraffic.class)).
+                        withName("net-traffic-logs")
+                        .withOffsetResetPolicy(Topology.AutoOffsetReset.LATEST));
+
+        sourceStream
+                .groupBy((k, v) -> v.getRemoteAddress(), Grouped.with(Serdes.String(), JsonSerdes.of(NetTraffic.class)))
+                .windowedBy(SessionWindows.ofInactivityGapWithNoGrace(Duration.ofMinutes(1)))
+                .count(Named.as("session-count"), Materialized.as("session-count-materialized"))
+                .suppress(Suppressed.untilWindowCloses(Suppressed.BufferConfig.unbounded()))
+                .toStream()
+                .foreach((k, v) -> LOGGER.info("The user:{} total visits {} times of website during {}-{}",
+                        k.key(), v, k.window().start(), k.window().end()));
+
+        return builder.build();
+    }
+}
